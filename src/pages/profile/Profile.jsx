4d24ab5a2dcc72e3ebe63a460c412c328fd03635
Abdulirhman Ashraf <sabdo3649@gmail.com>
2026-04-15 23:36:17 +0200

    feat: enhance Profile page; implement order pagination, display alert for no orders, and improve order table layout

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,16 +1,24 @@
 import { useAuth } from "../../context/AuthContext";
 import Buttons from "../../components/Buttons";
 import { useNavigate } from "react-router-dom";
-import "./profile.css";
 import { useFireStore } from "../../context/FireStoreContext";
-import { Table } from "react-bootstrap";
-import { useEffect } from "react";
+import { Alert, Table } from "react-bootstrap";
+import PaginationCom from "../../components/pagination/Pagination";
+import "./profile.css";
+import { useState } from "react";
 
 const Profile = () => {
   const navigate = useNavigate();
   const { currentUser, logout } = useAuth();
   const { userName, orderItems } = useFireStore();
- 
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const myOrders = orderItems.filter(
+    (items) => items.userId === currentUser?.uid,
+  );
+  const slicedProduct = 6;
+  const startIndex = (currentPage - 1) * slicedProduct;
+  const totalPages = Math.ceil(myOrders.length / slicedProduct);
   return (
     <div className="container profile">
       <h3 className="my-4">Welcome: {userName}</h3>
@@ -23,9 +31,11 @@ const Profile = () => {
       />
 
       <h3 className="text-center">Orders</h3>
+      {!myOrders.length ? <Alert className="text-center">No Orders</Alert>:
       <Table striped bordered hover>
         <thead>
           <tr>
+            <th>#</th>
             <th>Product</th>
             <td>Date/Time</td>
             <th>Price</th>
@@ -34,30 +44,39 @@ const Profile = () => {
           </tr>
         </thead>
         <tbody>
-          {orderItems?.map((items) =>
-            items.items?.map((item) => (
-              <tr key={item.id}>
-                <td>{item.name}</td>
-                <td>
-                  <div style={{ fontWeight: "bold" }}>
-                    {items.createAt?.toDate().toLocaleDateString("en-EG")}
-                  </div>
-                  <div style={{ fontSize: "12px", color: "gray" }}>
-                    {items.createAt?.toDate().toLocaleTimeString("en-EG")}
-                  </div>
-                </td>
-                <td>{item.price + "$"}</td>
-                <td>
-                  <img src={item.imageUrl} alt="" />
-                </td>
-                <td>
-                  <div>{items.status}</div>
-                </td>
-              </tr>
-            )),
-          )}
+          {myOrders
+            ?.slice(startIndex, startIndex + slicedProduct)
+            .map((items) =>
+              items.items?.map((item, index) => (
+                <tr key={item.id}>
+                  <td>{startIndex + index + 1}</td>
+                  <td>{item.name}</td>
+                  <td>
+                    <div style={{ fontWeight: "bold" }}>
+                      {items.createAt?.toDate().toLocaleDateString("en-EG")}
+                    </div>
+                    <div style={{ fontSize: "12px", color: "gray" }}>
+                      {items.createAt?.toDate().toLocaleTimeString("en-EG")}
+                    </div>
+                  </td>
+                  <td>{item.price + "$"}</td>
+                  <td>
+                    <img src={item.imageUrl} alt="" />
+                  </td>
+                  <td>
+                    <div>{items.status}</div>
+                  </td>
+                </tr>
+              )),
+            )}
         </tbody>
       </Table>
+    }
+      <PaginationCom
+        totalPages={totalPages}
+        active={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 };
